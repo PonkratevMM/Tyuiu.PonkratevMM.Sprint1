1939e3e6af8ff4ff9b747eb8570a61db244a37de
PonkratevMM <matvey190808@gmail.com>
2026-09-21 17:43:36 +0300

    Add project files.

---
+++ Tyuiu.PonkratevMM.Sprint1.Task0.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.PonkratevMM.Sprint1.Task0.V6.Lib;
+
+namespace Tyuiu.PonkratevMM.Sprint1.Task0.V6.Test
+
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds_Calculate();
+            Assert.AreEqual(2, res);
+        }
+
+        private int ds_Calculate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tyuiu.PonkratevMM.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.PonkratevMM.Sprint1.Task0.V6.Lib;
+namespace Tyuiu.PonkratevMM.Sprint1.Task0.V6
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил Понкратьев М.М | ИИПб-26";
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("* Спринт #1                                         *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту       *");
+            Console.WriteLine("* Задание #0                                        *");
+            Console.WriteLine("* Вариант #6                                        *");
+            Console.WriteLine("* Выполнил Понкратьев Матвей Максимович | ИИПб-26   *");
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                          *");
+            Console.WriteLine("* Написать программу, которая вычисляет             *");
+            Console.WriteLine("* выражение 3 * (4 + 1) и печатает резульатат      *");
+            Console.WriteLine("* на экран                                          *");
+            Console.WriteLine("*                                                   *");
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                  *");
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("* 3 * (4 + 1)                                       *");
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ                                          *");
+            Console.WriteLine("*****************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
