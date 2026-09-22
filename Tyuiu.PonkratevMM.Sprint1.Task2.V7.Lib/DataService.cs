404b9cfcc5db869e728f1109cc98b4d996f611f8
PonkratevMM <matvey190808@gmail.com>
2026-09-22 20:07:19 +0300

    редакт 4

--- Tyuiu.PonkratevMM.Sprint1.Task2.V7.Lib/DataService.cs
+++ Tyuiu.PonkratevMM.Sprint1.Task2.V7.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.PonkratevMM.Sprint1.Task2.V7.Lib
     {
         public double CalculateSquareCircle(int value)
         {
-            return Math.Pow(value ,2) * Math.PI;
+            return Math.Round(Math.Pow(value ,2) * Math.PI, 3);
            
         }
     }
