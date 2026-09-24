122ad21802bf45b6a7fd11ef3331c09c2cef4ce1
PonkratevMM <matvey190808@gmail.com>
2026-09-24 14:29:03 +0300

    Task5

---
+++ Tyuiu.PonkratevMM.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PonkratevMM.Sprint1.Task5.V6.Lib;
+
+namespace Tyuiu.PonkratevMM.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExprassion()
+        {
+            int k = 1;
+            DataService ds = new DataService();
+            int res = ds.Calculate(k);
+
+            Assert.AreEqual(1, res);
+        }
+    }
+}
--- Tyuiu.PonkratevMM.Sprint1.Task5.V6/Program.cs
+++ Tyuiu.PonkratevMM.Sprint1.Task5.V6/Program.cs
@@ -1,5 +1,5 @@
-﻿using Tyuiu.PonkratevMM.Sprint1.Task4.V1.Lib;
-namespace Tyuiu.PonkratevMM.Sprint1.Task4.V1
+﻿using Tyuiu.PonkratevMM.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.PonkratevMM.Sprint1.Task5.V1
 {
 
     class Program
@@ -12,33 +12,40 @@ namespace Tyuiu.PonkratevMM.Sprint1.Task4.V1
             Console.WriteLine("*****************************************************");
             Console.WriteLine("* Спринт #1                                         *");
             Console.WriteLine("* Тема: Базовые навыки работы в C#                  *");
-            Console.WriteLine("* Задание #4                                        *");
+            Console.WriteLine("* Задание #5                                        *");
             Console.WriteLine("* Вариант #6                                        *");
             Console.WriteLine("* Выполнил Понкратьев Матвей Максимович | ИИПб-26   *");
             Console.WriteLine("*****************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                          *");
-            Console.WriteLine("* Написать программу, которая запрашивает           *");
-            Console.WriteLine("* у пользователя исходные данные, вычисляет         *");
-            Console.WriteLine("* результат по формуле 1/(x+2)^2 и печатает         *");
-            Console.WriteLine("* его на экране.                                    *");
+            Console.WriteLine("* Написать программу, которая решает следующую      *");
+            Console.WriteLine("* задачу: Пусть k – целое от 1 до 365.              *");
+            Console.WriteLine("* Присвоить целой переменной n значение 1,2,...,7   *");
+            Console.WriteLine("* в зависимости от того, на какой день недели       *");
+            Console.WriteLine("* приходится k-й день невисокосного года, в котором *");
+            Console.WriteLine("* 1 января – понедельник.                           *");
             Console.WriteLine("*****************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                  *");
             Console.WriteLine("*****************************************************");
 
-            int x;
-            Console.WriteLine("Введите значение x:");
-            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите k (1-365): ");
+            int k = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("*****************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                        *");
             Console.WriteLine("*****************************************************");
 
-            Console.WriteLine("1 / (x + 2)^2 = " + ds.Calculate(x));
+            if (k >= 1 && k <= 365)
+            {
+                int n = (k - 1) % 7 + 1;
+                Console.WriteLine("n = " + n);
+            }
+            else
+            {
+                Console.WriteLine("Ошибка");
+            }
+
+
             Console.ReadKey();
-
-            
-
-
 
         }
     }
